470c4fc05797f08c18942622f051edc670a9494d
Zhao Yu Lin <zlin20@stuy.edu>
2022-06-10 03:33:02 -0400

    animations

--- tas/static/js/rewards.js
+++ tas/static/js/rewards.js
@@ -8,9 +8,7 @@ function myScore() {
   console.log("your score is : " + score);
   return score;
 }
-if (score == 5) {
-  console.log("you socre are five");
-}
+
 const canvas = document.getElementById("canvas1");
 myScore();
 
@@ -22,10 +20,87 @@ playerImage.src = "../../static/images/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
-let frameY = 0;
+let frameY = -1;
+let limit = -1;
+let staggerFrames = -1;
 let gameFrame = 0;
-const staggerFrames = 5;
-console.log("hi!");
+
+const dogState = [
+  {
+    name: "idle",
+    frameY: 0,
+    limit: 6,
+    staggerFrames: 7,
+  },
+  {
+    name: "run",
+    frameY: 3,
+    limit: 6,
+    staggerFrames: 7,
+  },
+  {
+    name: "dazed",
+    frameY: 4,
+    limit: 10,
+    staggerFrames: 7,
+  },
+  {
+    name: "roll",
+    frameY: 6,
+    limit: 6,
+    staggerFrames: 7,
+  },
+  {
+    name: "bite",
+    frameY: 7,
+    limit: 6,
+    staggerFrames: 7,
+  },
+  {
+    name: "derp",
+    frameY: 8,
+    limit: 11,
+    staggerFrames: 10,
+  },
+  {
+    name: "stanky-leg",
+    frameY: 9,
+    limit: 3,
+    staggerFrames: 10,
+  },
+];
+
+function playAnimation(number) {
+  frameY = dogState[number].frameY;
+  limit = dogState[number].limit;
+  staggerFrames = dogState[number].staggerFrames;
+  return [frameY, limit, staggerFrames];
+}
+//animation = get stuff from drop down menu
+animation = "stanky-leg";
+if (animation == "idle") {
+  playAnimation(0);
+  //   console.log(setFrame(0));
+}
+if (animation == "run") {
+  playAnimation(1);
+}
+if (animation == "dazed") {
+  playAnimation(2);
+}
+if (animation == "roll") {
+  playAnimation(3);
+}
+if (animation == "bite") {
+  playAnimation(4);
+}
+if (animation == "derp") {
+  playAnimation(5);
+}
+if (animation == "stanky-leg") {
+  playAnimation(6);
+}
+
 function animate() {
   console.log("animating!");
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -44,7 +119,7 @@ function animate() {
   );
   //   ctx.drawImage(playerImage, 0, 0);
   if (gameFrame % staggerFrames == 0) {
-    if (frameX < 6) frameX++;
+    if (frameX < limit) frameX++;
     else frameX = 0;
   }
   gameFrame++;
